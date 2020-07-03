5703fcd0915cd7a3d4b03b0021753e03ebb3a1cb
jeXMin <rejesthm@gmail.com>
2020-07-03 23:30:59 +0800

    CHORE: Update group member model

--- app/API/v1/Models/GroupMembers.php
+++ app/API/v1/Models/GroupMembers.php
@@ -3,8 +3,20 @@
 namespace App\API\v1\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class GroupMembers extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'group_id', 'user_id', 'user_position'
+    ];
+
 }
