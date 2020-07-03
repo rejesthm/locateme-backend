e0eb07269ea3bec846d86e408c188ac191629544
jeXMin <rejesthm@gmail.com>
2020-07-03 23:31:27 +0800

    CHORE: Update functionality of creating group

--- app/API/v1/Controllers/CreateGroup/CreateGroupController.php
+++ app/API/v1/Controllers/CreateGroup/CreateGroupController.php
@@ -8,6 +8,7 @@ use App\API\v1\Services\helpers\UploadFileHelper;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 
 class CreateGroupController extends Controller
@@ -20,18 +21,21 @@ class CreateGroupController extends Controller
             'group_destination_name'    => $request->input('group_destination_name'),
             'destination_lat'    => $request->input('group_destination_lat'),
             'destination_long'    => $request->input('group_destination_long'),
-            'creator_id'    => Auth::user()->id
+            'creator_id'    => Auth::user()->id,
+            'members'   => $request->input('members'),
         ];
-
         $fileHelper = new UploadFileHelper("groups");
         $url = $fileHelper->uploadFile($request->file('group_image'), $data['group_destination_name']);
 
+        DB::beginTransaction();
         try {
             $service = new CreateGroupService();
             $data['group_image_url'] = $url;
             $response = $service->actionGroup($data);
-            return $this->standardResponse('Group Registered Successfuly', [$response]);
+            DB::commit();
+            return $this->standardResponse('Group Registered Successfuly', $response);
         } catch (\Exception $e) {
+            DB::rollback();
             File::delete($url);
             return $this->standardResponse($e->getMessage(), $data, $e->getCode(), $e->getTrace());
         }
--- app/API/v1/Services/CreateGroupService.php
+++ app/API/v1/Services/CreateGroupService.php
@@ -3,18 +3,20 @@
 
 namespace App\API\v1\Services;
 
+use App\API\v1\Models\GroupMembers;
 use App\API\v1\Models\Groups;
 
 class CreateGroupService
 {
     public function actionGroup($data)
     {
-        if(!empty($data['id'])){
+        if (!empty($data['id'])) {
             $model = Groups::query()->find($data['id']);
         }
         if (empty($model)) {
             $model = new Groups();
         }
+
         $model->group_name = $data['group_name'];
         $model->group_destination = $data['group_destination_name'];
         $model->destination_lat = $data['destination_lat'];
@@ -24,8 +26,24 @@ class CreateGroupService
         $model->date = now();
 
         if (!$model->save()) {
-            throw new \Exception("Unable to save group", 500);
+            throw new \Exception("Unable to save group " . $data['group_name'], 500);
         }
+        $this->saveMembers($data['members'], $model->id);
+
         return $model;
+    }
+
+    public function saveMembers($members, $groupId)
+    {
+
+        foreach ($members as $member) {
+            $membersModel = new GroupMembers();
+            $membersModel->group_id = $groupId;
+            $membersModel->user_id = $member['user_id'];
+            $membersModel->user_position = 'member';
+            if (!$membersModel->save()) {
+                throw new \Exception("Unable to save " . $member . " from members", 500);
+            }
+        }
     }
 }
